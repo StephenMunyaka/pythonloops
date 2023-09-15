9da555d343a725f148a66e7a5de2f2d2284e8f79
Stevorithee@gmail.com <stevorithee@gmail.com>
2023-09-15 09:49:55 +0300

    pythonloops

---
+++ loop2.py
@@ -0,0 +1,8 @@
+age = int(input("What is you age? "))
+if age > 18:
+    converted = age + 2
+    print("age is ", converted)
+else:
+    converted = age - 2
+    print("age is ",converted)    
+    
